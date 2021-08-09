269653e70684689ff380a77fa23d60bc91f07385
Solaiman Shadin <solaimanshadin@gmail.com>
2021-08-09 22:20:22 +0600

    [ADD] precommit hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
-
-function App () {
-  return (
-    <div>
-      
-    </div>
-  )
+function App() {
+  return <div></div>;
 }
 
-export default App
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import reportWebVitals from './reportWebVitals'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-        <App />
+    <App />
+    <h1></h1>
   </React.StrictMode>,
   document.getElementById('root')
-)
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+reportWebVitals();
