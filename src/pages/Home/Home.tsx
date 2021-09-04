7a5e7808d7aefea339cf0b5e3a1884743a68bc18
Md. Solaiman Shadin <37359371+solaimanshadin@users.noreply.github.com>
2021-09-04 20:34:10 +0600

    Revert "[ADD] shop by store"

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,9 @@
 import Banner from 'components/home/Banner';
 import Products from 'components/home/Products';
-import ShopByStore from 'components/home/ShopByStore';
 import useAsync from 'hooks/useAsync';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 // Was a mistake in import:  import { useEffect } from 'react-dom/node_modules/@types/react';
 import ProductService from 'services/ProductService';
-import StoreService from 'services/StoreService';
-import { IStore } from 'types';
 
 const Home = () => {
   // const [products, setProducts] = useState<IProduct[]>([]);
@@ -15,23 +12,12 @@ const Home = () => {
   //   ProductService.getProducts().then((res) => setProducts(res));
   // }, []);
 
-  const [storeData, setStoreData] = useState<IStore[]>([]);
-
-  useEffect(() => {
-    StoreService.getStores().then((res) => setStoreData(res));
-  }, []);
-
-  const { data, isLoading, isSuccess } = useAsync(ProductService.getProducts);
+  const { data, isLoading } = useAsync(ProductService.getProducts);
 
   return (
     <div>
       <Banner />
       <Products isLoading={isLoading} products={data} />
-      <ShopByStore
-        isSuccess={isSuccess}
-        isLoading={isLoading}
-        shopByStore={storeData}
-      />
     </div>
   );
 };
