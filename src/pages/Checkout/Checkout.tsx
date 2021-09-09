03a64f44643462ea37045ec0d97e12f27e54000e
Solaiman Shadin <solaimanshadin@gmail.com>
2021-09-09 18:42:33 +0600

    [ADD] Remove from cart functionality

--- src/pages/Checkout/Checkout.tsx
+++ src/pages/Checkout/Checkout.tsx
@@ -1,23 +1,29 @@
 import CartItem from 'components/checkout/CartItem';
 import React from 'react';
 import { Container } from 'react-bootstrap';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { clearCart } from 'redux/actionCreators/cartAction';
 import { AppState } from 'redux/store';
-import { IProduct } from 'types';
 
 const Checkout = () => {
-  const cart: IProduct[] = useSelector((state: AppState) => state.cart);
-
+  const dispatch = useDispatch();
+  const cart = useSelector((state: AppState) => state.cart);
+  const cartTotal = cart.reduce((acc, crr) => acc + crr.price, 0);
   return (
     <div className=" my-3">
       <Container>
         <div className="wrapper bg-white rounded border p-5">
           {cart.map((item) => (
-            <CartItem key={item._id} />
+            <CartItem key={item._id} item={item} />
           ))}
           <div className="mt-5 d-flex flex-column align-items-end border-top pt-5">
-            <h2 className="mb-4">Total : ৳ 2343</h2>
-            <button className="btn btn-lg btn-primary">Confirm Checkout</button>
+            <h2 className="mb-4">Total : ৳ {cartTotal}</h2>
+            <button
+              onClick={() => dispatch(clearCart())}
+              className="btn btn-lg btn-primary"
+            >
+              Confirm Checkout
+            </button>
           </div>
         </div>
       </Container>
